65ad5d164f39bdb606e148070517de0110858068
crycetruly <crycetruly@gmail.com>
2019-09-21 20:25:12 +0300

    ch(setup): setup react from scratch
    - use the react scripts package to manage the mandane tasks like hot jsx transpiling
    
    [#1]

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Form from './Form';
+
+
+function App(){
+return (<div>
+    <h2>Hello from react hehe</h2>
+    <Form/>
+</div>
+);
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'))
